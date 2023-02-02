73a3c3792eb34802c9663cb6fb8f1a3277c2235f
Evolusin <98921577+Evolusin@users.noreply.github.com>
2023-02-02 19:04:54 +0100

    Moved to scan all pages

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ class Product:
         soup = BeautifulSoup(page.content, "html.parser")
         # Search for bdi tag inside of section class
         self.price = soup.find("section", class_="elementor-section elementor-top-section elementor-element elementor-element-2bdb3ad elementor-section-boxed elementor-section-height-default elementor-section-height-default").find("bdi").text
-
+        # strip price from unnecessary characters
+        self.price = self.price.replace("zł", "").replace(" ", "").replace(",", ".")
             
 class CSV:
     def __init__(self):
@@ -38,20 +39,32 @@ class CSV:
             writer.writerow([product.title, product.href, product.price])
             
 
-url = "https://czteryszuflady.pl/sklep/kategorie/pokemon-tcg/"
+def get_all_items(url):
+    # Pobieranie strony
+    page = requests.get(url)
 
-# Pobieranie strony
-page = requests.get(url)
+    # Tworzenie obiektu BeautifulSoup
+    soup = BeautifulSoup(page.content, "html.parser")
 
-# Tworzenie obiektu BeautifulSoup
-soup = BeautifulSoup(page.content, "html.parser")
+    # Search for all products by div class
+    products = soup.find_all("div", class_="woocommerce-loop-product__title")
+    saver = CSV()
+    for x in products:
+        product = Product(x.text, x.find("a")["href"])
+        product.scrap_price()
+        # append product to csv file
+        saver.save(product)
+        
+def get_all_pages():
+    # Get all pages from main page
+    page = requests.get("https://czteryszuflady.pl/sklep/kategorie/pokemon-tcg/page/1/")
+    soup = BeautifulSoup(page.content, "html.parser")
+    pages = soup.find("ul", class_="page-numbers").find_all("li")
+    # extract only html links from pages
+    for x in pages:
+        if x.find("a") != None:
+            print(x.find("a")["href"])
+    # remove diu
 
-# Search for all products by div class
-products = soup.find_all("div", class_="woocommerce-loop-product__title")
-saver = CSV()
-for x in products:
-    product = Product(x.text, x.find("a")["href"])
-    product.scrap_price()
-    # append product to csv file
-    saver.save(product)
-    
+
+get_all_pages()
