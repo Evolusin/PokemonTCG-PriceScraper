1eba080dc7421a2a4aa2a9bb6f0af186c11946bb
Evolusin <evoarcadias@gmail.com>
2023-02-02 21:12:27 +0000

    Added scrapper sh

---
+++ utils/scrapper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Activate virtual environment
+VENV_PATH=/home/django/Desktop/programing/PokemonTCG-PriceScraper/venv/bin/activate
+source $VENV_PATH/bin/activate
+
+# Run Python script
+python /home/django/Desktop/programing/PokemonTCG-PriceScraper/main.py
+
+# Deactivate virtual environment
+deactivate
